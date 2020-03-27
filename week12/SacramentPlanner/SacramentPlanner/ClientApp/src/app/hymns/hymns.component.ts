d93979ad3efbc3de88954b440f5e8dbe66a0dbe8
Alex Shnyrov <1993alexey@gmail.com>
2020-03-26 21:01:00 -0600

    Added hymn component

--- week12/SacramentPlanner/SacramentPlanner/ClientApp/src/app/hymns/hymns.component.ts
+++ week12/SacramentPlanner/SacramentPlanner/ClientApp/src/app/hymns/hymns.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HymnService } from '../services/hymn.service';
 import { Hymn } from '../models/hymn.model';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-hymns',
@@ -9,11 +10,33 @@ import { Hymn } from '../models/hymn.model';
 })
 export class HymnsComponent implements OnInit {
 
+  faTrashAlt = faTrashAlt
+  faEdit = faEdit
   public hymns: Hymn[]
+
   constructor(private hymnsService: HymnService) { }
 
   ngOnInit() {
-    this.hymnsService.getHymns().subscribe(hymns => this.hymns = hymns)
+    this.hymnsService.getHymns().subscribe(hymns => {
+      this.hymns = hymns
+      this.sortHymns()
+    })
   }
 
+  create(hymn: Hymn) {
+    this.hymnsService.addHymn(hymn)
+      .subscribe(newHymn => {
+        this.hymns.push(newHymn)
+        this.sortHymns()
+      })
+  }
+
+  delete(id: string) {
+    this.hymns = this.hymns.filter(hymn => hymn.id != id)
+    this.hymnsService.deleteHymn(id).subscribe()
+  }
+
+  sortHymns() {
+    this.hymns.sort(((a, b) => a.number - b.number))
+  }
 }

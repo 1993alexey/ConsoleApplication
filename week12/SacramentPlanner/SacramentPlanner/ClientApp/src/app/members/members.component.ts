cc7f38f20aa01261f74ae738dbd46202ba3b919e
Slava Nazarenko <slavaecu@gmail.com>
2020-03-27 13:26:53 -0600

    member component is done

--- week12/SacramentPlanner/SacramentPlanner/ClientApp/src/app/members/members.component.ts
+++ week12/SacramentPlanner/SacramentPlanner/ClientApp/src/app/members/members.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MemberService } from './member.service';
+import { Member } from './member.model';
+import { faTrashAlt, faEdit } from '@fortawesome/free-solid-svg-icons';
+declare var $: any;
 
 @Component({
   selector: 'app-members',
@@ -6,10 +10,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./members.component.css']
 })
 export class MembersComponent implements OnInit {
+  @ViewChild('firstName', { static: true }) memberFirstNameInput: any;
+  @ViewChild('lastName', { static: true }) memberlastNameInput: any;
+  @ViewChild('memberTitle', { static: true }) memberTitleInput: any;
 
-  constructor() { }
+  faTrashAlt = faTrashAlt
+  faEdit = faEdit
+  public members: Member[]
+
+  constructor(private memberService: MemberService) { }
 
   ngOnInit() {
+    this.memberService.getMembers().subscribe(members => {
+      this.members = members
+      this.sortMembers()
+    })
+  }
+
+  sortMembers() {
+    this.members.sort(((a, b) => a.lastName > b.lastName ? 1 : -1))
+  }
+
+  create(member: Member) {
+    this.memberService.addMember(member)
+      .subscribe(newMember => {
+        this.members.push(newMember)
+        this.sortMembers()
+      })
+
+    this.memberService.getMembers().subscribe(members => this.members = members)
+  }
+
+  delete(id: string) {
+    this.members = this.members.filter(member => member.id != id)
+    this.memberService.deleteMember(id).subscribe()
+  }
+
+  hideModal() {
+    this.memberFirstNameInput.nativeElement.value = ''
+    this.memberlastNameInput.nativeElement.value = ''
+    this.memberTitleInput.nativeElement.value = ''
+    $("#member-modal").modal("hide");
   }
 
 }
